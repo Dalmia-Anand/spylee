dd2843945ad025c20d4a83438baaf221191f2fef
Anand Dalmia <adanandkumar12@gmail.com>
2022-02-14 01:16:45 +0530

    commit

--- app/src/main/java/com/spylee/SimpleMail.java
+++ app/src/main/java/com/spylee/SimpleMail.java
@@ -4,10 +4,12 @@ import java.util.Properties;
 
 import javax.mail.BodyPart;
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
@@ -23,6 +25,9 @@ public class SimpleMail {
     private static final String SMTP_HOST_NAME = "smtp.gmail.com"; //can be your host server smtp@yourdomain.com
     private static final String SMTP_AUTH_USER = "ananddalmia44@gmail.com"; //your login username/email
     private static final String SMTP_AUTH_PWD  = "anand@70049"; //password/secret
+    private static String from = "ananddalmia44@gmail.com"; //from
+    private static final String username = SMTP_AUTH_USER;
+    private static final String password = SMTP_AUTH_PWD;
 
     private static Message message;
 
@@ -31,19 +36,19 @@ public class SimpleMail {
         // Recipient's email ID needs to be mentioned.
 
         // Sender's email ID needs to be mentioned
-        String from = "ananddalmia44@gmail.com"; //from
 
-        final String username = SMTP_AUTH_USER;
-        final String password = SMTP_AUTH_PWD;
+
 
         // Assuming you are sending email through relay.jangosmtp.net
         String host = SMTP_HOST_NAME;
 
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
 
         // Get the Session object.
         Session session = Session.getInstance(props,
@@ -53,12 +58,34 @@ public class SimpleMail {
                     }
                 });
 
-        try {
+      /*  try {
+            //Creating MimeMessage object
+            MimeMessage mm = new MimeMessage(session);
+
+            //Setting sender address
+            mm.setFrom(new InternetAddress(username));
+            //Adding receiver
+            mm.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            //Adding subject
+            mm.setSubject(subject);
+            //Adding message
+
+            mm.setText(msg);
+
+            //Sending email
+            Transport.send(mm);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }*/
+
+
+       try {
             // Create a default MimeMessage object.
             message = new MimeMessage(session);
 
             // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
+            message.setFrom(new InternetAddress(username));
 
             // Set To: header field of the header.
             message.setRecipients(Message.RecipientType.TO,
@@ -111,5 +138,6 @@ public class SimpleMail {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
     }
 }
